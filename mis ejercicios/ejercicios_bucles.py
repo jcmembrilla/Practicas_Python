f80048af6690676c040fb1843471b9f8df372d89
Sandra <estudiodivasson@gmail.com>
2025-11-18 20:52:04 -0300

    Resolvi ejercicios bucles for.

--- mis ejercicios/ejercicios_bucles.py
+++ mis ejercicios/ejercicios_bucles.py
@@ -40,10 +40,46 @@ Debe mostrar al final: "La suma total es: X", donde X es el resultado de la suma
  pero detenga el bucle con break si el número es igual a 6.
  Muestra un mensaje que diga "Bucle interrumpido en el número 6".
 """
-contador = 1
-while contador <11:
-    print(contador)
-    contador +=1
-    if contador ==6:
-        print("Bucle interrumpido en el número 6")
-        break
+# contador = 1
+# while contador <11:
+#     print(contador)
+#     contador +=1
+#     if contador ==6:
+#         print("Bucle interrumpido en el número 6")
+#         break
+
+"""
+Ejercicios for:
+"""
+"""
+Crea una lista llamada animales que contenga tres nombres de animales.
+Luego, usa un bucle for para recorrer la lista e imprimir cada uno en pantalla.
+"""
+# animales = ["mono", "burro", "gato"]
+# for animal in animales:
+#     print(animal)
+"""
+Utiliza range() para mostrar los números del 1 al 10, ambos inclusive,
+ usando un bucle for y el formato de impresión:
+ El número es: X.
+"""
+# for num in range(1, 11):
+#     print(f"El número es: {num}")
+"""
+Crea una cadena de texto con tu nombre y usa un bucle for para recorrerla,
+mostrando cada letra por separado en una nueva línea.
+"""
+# cadena = "Sandra Divasson"
+# for caracter in cadena:
+#     print(caracter)
+"""
+Usando range(), realiza un bucle for que muestre las siguientes líneas:
+Iteración número: 0 
+Iteración número: 1 
+Iteración número: 2 
+Iteración número: 3 
+Iteración número: 4
+Pista: el bucle debe repetirse cinco veces.
+"""
+for i in range(5):
+    print(f"Iteración número: {i}")
