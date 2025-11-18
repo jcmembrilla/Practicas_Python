2b49a0203909273b4ccee91e1aca33744b0430e5
Nombre Apellido <gabrielortino@yahoo.com.ar>
2025-11-18 20:50:08 -0300

    ejercicios tuplas

---
+++ ejercicios_gabriel/ejercicios_tuplas.py
@@ -0,0 +1,26 @@
+#1. Inmutabilidad:
+#Creá una tupla llamada numeros con cinco valores.
+#Intentá cambiar el segundo elemento por otro valor y observá el error que aparece.
+
+numeros = (1,2,3,4,5)
+print(numeros)
+#numeros(0) = 20
+#print(numeros)
+
+
+#2. Acceso y recorrido:
+#Creá una tupla llamada animales con tres nombres de animales.
+#a) Mostrá el primer y el último elemento.
+#b) Recorrela con un bucle for para imprimir cada animal.
+
+animales = ("gato","perro","raton")
+print(animales[0],animales[2])
+
+
+#3. Conversión lista <-> tupla:
+#Convertí la tupla animales en una lista, agregá un nuevo elemento, y luego volvé a convertirla en tupla.
+#Mostrá la tupla resultante.
+
+#4. Uso como clave en un diccionario:
+#Creá un diccionario llamado coordenadas donde las claves sean tuplas que representen posiciones (x, y) y los valores sean nombres de ciudades.
+#Luego imprimí el valor asociado a una de esas coordenadas.
