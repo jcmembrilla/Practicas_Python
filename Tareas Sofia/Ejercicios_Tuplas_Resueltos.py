1b7ee0e3745bcd60b73105863e0689cf7f90bfc7
Sofia Cestona <cestonasofi0@gmail.com>
2025-11-27 00:41:01 -0300

    termine los ejercicios de tuplas

--- Tareas Sofia/Ejercicios_Tuplas_Resueltos.py
+++ Tareas Sofia/Ejercicios_Tuplas_Resueltos.py
@@ -4,10 +4,11 @@ Creá una tupla llamada numeros con cinco valores.
 Intentá cambiar el segundo elemento por otro valor y observá el error que aparece.
 """
 
-tuple = (10,20,30,40,50)
-#tuple.remove(1)
-#tuple.insert(1,25)
-print(tuple) 
+numeros = (10,20,30,40,50)
+#numeros.remove(1)
+#numeros.insert(1,25)
+print(numeros) 
+#no entiendo bien, no funciona
 
 
 #2. Acceso y recorrido:
@@ -19,10 +20,10 @@ b) Recorrela con un bucle for para imprimir cada animal.
 
 animales_tuple = ("gato","perro","conejo")
 
-print(tuple[0])
-print(tuple[2])
+print(animales_tuple[0])
+print(animales_tuple[-1])
 
-for animales in tuple:
+for animales in animales_tuple:
     print(animales)
 
 #3. Conversión lista <-> tupla:
@@ -31,12 +32,16 @@ Convertí la tupla animales en una lista, agregá un nuevo elemento, y luego vol
 en tupla.
 Mostrá la tupla resultante.
 """
-animales = ["gato", "perro", "conejo"]
-animales.insert(0, "serpiente")
-print(animales)
+animales = ("gato", "perro", "conejo") #esto es una tupla!!!!
 
-tuple = animales 
+animales = [animales]# esto es una lista...
+animales.append("serpiente") #le agrego otro elemento con "append" que sirve para agregar elementos al final de la lista
+animales = (animales) #esto vuelve a ser una tupla...
+
 print(animales)
+#los corchetes son para las listas, los parentesis para las tuplas
+
+
 
 #4. Uso como clave en un diccionario:
 """
@@ -44,3 +49,10 @@ Creá un diccionario llamado coordenadas donde las claves sean tuplas que repres
 Luego imprimí el valor asociado a una de esas coordenadas.
 """
 
+coordenadas = {(10,20) : "buenos aires", (20,30) : "cordoba"}
+print(coordenadas[(10,20)])
+#las claves son key 
+#los valores son value
+
+#usan llaves para definir un diccionarios y los corchetes para los valores!....
+# y dos puntos para separar, ------> clave:valor
